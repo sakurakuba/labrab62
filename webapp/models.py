6bcc9021f31e467ea4e3727710ed7a5b3c843d91
Kuba <ksulaymanov@gmail.com>
2022-08-22 23:53:32 -0500

    cloned repo shop

---
+++ webapp/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+DEFAULT_CATEGORY = 'other'
+CATEGORY_CHOICES = (
+    (DEFAULT_CATEGORY, 'Разное'),
+    ('food', 'Еда'),
+    ('tech', 'Бытовая техника'),
+    ('tools', 'Инструменты'),
+    ('toys', 'Игрушки'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES,
+                                verbose_name='Категория')
+    amount = models.PositiveIntegerField(verbose_name='Остаток')
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2,
+                                validators=(MinValueValidator(0),))
+
+    def __str__(self):
+        return f'{self.name} - {self.amount}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class Cart(models.Model):
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE,
+                                verbose_name='Товар', related_name='in_cart')
+    qty = models.PositiveIntegerField(verbose_name='Количество', default=1)
+
+    def __str__(self):
+        return f'{self.product.name} - {self.qty}'
+
+    class Meta:
+        verbose_name = 'Товар в корзине'
+        verbose_name_plural = 'Товары в корзине'
+
+    def get_product_total(self):
+        return self.qty * self.product.price
+
+    @classmethod
+    def get_total(cls):
+        total = 0
+        for cart in cls.objects.all():
+            total += cart.get_product_total()
+        return total
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    phone = models.CharField(max_length=30, verbose_name='Телефон')
+    address = models.CharField(max_length=100, verbose_name='Адрес')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    products = models.ManyToManyField('webapp.Product', related_name='orders', verbose_name='Товары',
+                                      through='webapp.OrderProduct', through_fields=['order', 'product'])
+
+    def __str__(self):
+        return f'{self.name} - {self.phone}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class OrderProduct(models.Model):
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE,
+                                verbose_name='Товар', related_name='order_products')
+    order = models.ForeignKey('webapp.Order', on_delete=models.CASCADE,
+                              verbose_name='Заказ', related_name='order_products')
+    qty = models.PositiveIntegerField(verbose_name='Количество')
+
+    def __str__(self):
+        return f'{self.product.name} - {self.order.name}'
+
+    class Meta:
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
